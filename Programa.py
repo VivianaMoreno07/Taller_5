6bd02b43f5728a1046dd7fdfe392202c5cbe33ee
Viviana Moreno <vmorenoo@udistrital.edu.co>
2024-04-15 15:12:34 -0500

    Numpy_3

--- Programa.py
+++ Programa.py
@@ -1,1 +1,19 @@
-#Taller Numpy
+#Taller Numpy:Biblioteca para realizar cálculos numéricos en arreglos n dimensionales
+
+import numpy as np
+
+#Crear un ndarray de 2 dimensiones a partir de una lista
+
+A= np.array([[1,5], [7,9]])
+B= np.array([[2,0], [1,3]])
+
+#Producto punto entre ndarrays
+
+C=np.dot(A,B)
+print(C)
+
+#Solucion de un sistema de ecuaciones con numpy
+solucion=np.array([5,17])
+
+m=np.linalg.solve(A,solucion)
+print(m)
